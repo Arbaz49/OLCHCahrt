846693a095938a2d575dcdcd94966a8c62f0ce42
Arbaz <arbaz24.3.20@gmail.com>
2023-06-14 08:41:26 +0530

    oredrbook Table optimized

--- src/components/OrderBookTable.tsx
+++ src/components/OrderBookTable.tsx
@@ -1,101 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-// /* eslint-disable react/jsx-no-comment-textnodes */
-// /* eslint-disable react/react-in-jsx-scope */
-// // import React,{useEffect} from 'react';
-// // <<<<<<< HEAD
-
-// import { OrderProps } from "../types/DataType";
-// import { useEffect} from "react";
-// import { WS_URL } from "../utils/Constants";
-// import {
-//   // asksArray,
-//   asksMap,
-//   // bidsArray,
-//   bidsMap,
-//   updateMap,
-// } from "../utils/OrderBookservice";
-// const w = new WebSocket(WS_URL);
-
-// export default function OrderBookTable({ selectedCoin }: OrderProps) {
-//   // const bidsMap=new Map<number,[number,number,number]>()
-
-//   // const [asksData, setAsksData] = useState<any[]>([]);
-
-//   useEffect(() => {
-//     const msg = JSON.stringify({
-//       event: "subscribe",
-//       channel: "book",
-//       symbol: selectedCoin,
-//     });
-//     w.onopen = (): void => {
-//       w.send(msg);
-//     };
-//     w.onmessage = (a: MessageEvent<string>): void => {
-//       const _array = JSON.parse(a.data);
-//       // console.log('socketArray',_array);
-//       if (Array.isArray(_array) && _array[1] !== "hb") {
-//         updateMap(_array[1]);
-//       }
-//     };
-//   }, []);
-//   // console.log("asksdata", asksMap);
-//   // console.log("bidsarray", bidsArray);
-//   return (
-
-//     <>
-//     {/* <button>Back To Home</button> */}
-//       <div style={{ display: "flex",width:"40vw",margin:"auto" }}>
-//         <table border={0} style={{padding:"10px"}}>
-//           <tr style={{ backgroundColor: "aqua" }}>
-//             <td>Count</td>
-//             <td>Amount</td>
-//             <td>Total</td>
-//             <td>price</td>
-//           </tr>
-
-//           {[...bidsMap].reverse().slice(1, 21).map(([key,value], index) => {
-//             return (
-//               <>
-//                 {index > 0 && (
-//                   <tr>
-//                     <td>{value[0]}</td>
-//                     <td>{value[1]}</td>
-//                     <td>{value[2]}</td>
-//                     <td>{value[3]}</td>
-//                   </tr>
-//                 )}
-//               </>
-//             );
-//           })}
-//         </table>
-//         {/* <table border={0}style={{}}>
-//           <tr style={{ backgroundColor: "aqua" }}>
-//             <td>price</td>
-//             <td>Total</td>
-//             <td>Amount</td>
-//             <td>Count</td>
-//           </tr>
-
-//           {asksArray.slice(1, 21).map((ele:any, index) => {
-//             return (
-//               <>
-//                 {index > 0 && (
-//                   <tr>
-//                     <td>{ele[3]}</td>
-//                     <td>{ele[2]}</td>
-//                     <td>{ele[1]}</td>
-//                     <td>{ele[0]}</td>
-//                   </tr>
-//                 )}
-//               </>
-//             );
-//           })}
-//         </table> */}
-//       </div>
-//     </>
-
-//   )}
-
 import React, { useEffect, useState } from "react";
 import {
   Index_Of_Amount,
@@ -105,22 +7,20 @@ import {
   OrderProps,
 } from "../types/DataType";
 import { WS_URL } from "../utils/Constants";
-import { asksMap, bidsMap, calculateTotal, updateMap } from "../utils/OrderBookservice";
+import {
+  asksMap,
+  bidsMap,
+  calculateTotal,
+  updateMap,
+} from "../utils/OrderBookservice";
 import { useParams } from "react-router-dom";
-// import { asksMap, bidsMap,} from "../utils/OrderBookServices";
-// import { bidsMap, updateMap } from "../utils/OrderBookservice";
 
 const w = new WebSocket(WS_URL);
 
 export default function OrderBookTable({ selectedCoin }: OrderProps) {
-  // const [asksData, setAsksData] = useState<any[]>([]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // const bidsMap = new Map<number, [number, number, number, number]>();
-  // const asksMap = new Map<number, [number, number, number, number]>();
-  // const [bidsMapData, setBidsMapData] =
-    // useState<Map<number, [number, number, number, number]>>(bidsMap);
+  const [bidsTableData, setBidsTableData] = useState<any[]>([]);
+  const [asksTableData, setAsksTableData] = useState<any[]>([]);
 
-  const [bidstest, setbidsTest] = useState<any[]>([]);
   const { coinid } = useParams();
 
   const [wsEvent, setWsEvent] = useState("subscribe");
@@ -141,13 +41,13 @@ export default function OrderBookTable({ selectedCoin }: OrderProps) {
       setChanId(_array[0]);
       if (Array.isArray(_array) && _array[1] !== "hb") {
         updateMap(_array[1], asksMap, bidsMap);
-        setbidsTest([...bidstest, ...bidsMap]);
+        setBidsTableData([...bidsTableData, ...bidsMap]);
+        setAsksTableData([...asksTableData, ...asksMap]);
       }
     };
-
   }, [asksMap, bidsMap, coinid, selectedCoin]);
-  console.log("bidsmapkeys", bidsMap);
-  const bids=[...bidstest]
+  const bids = [...bidsTableData];
+  const asks = [...asksTableData];
   return (
     <>
       <div style={{ display: "flex", width: "40vw", margin: "auto" }}>
@@ -159,69 +59,71 @@ export default function OrderBookTable({ selectedCoin }: OrderProps) {
             <td style={{ height: "30px" }}>Total</td>
             <td style={{ height: "30px" }}>Price</td>
           </tr>
-          {[...bidstest]
-            .sort(function (a, b) {
-              if (a[1][3] === b[1][3])  return 0;
-               else   return a[1][3] < b[1][3] ? 1 : -1;  
-            })
+          {[...bidsTableData]
+            .sort((currentRow, nextRow)=>{
+              if (currentRow[1][Index_Of_Price] === nextRow[1][Index_Of_Price]) return 0;
+              else return currentRow[1][3] < nextRow[1][3] ? 1 : -1})
             .slice(1, 21)
-            .map(([key, value], index) => (
-              <tr key={key + index}>
-                <td>{value[Index_Of_Count]}</td>
-                <td>{value[Index_Of_Amount].toFixed(3)}</td>
-                <td>{calculateTotal(index,bids.sort(function (a, b) {
-              if (a[1][3] === b[1][3])  return 0;
-               else   return a[1][3] < b[1][3] ? 1 : -1;  
-            }),value,"bids")}{value[Index_Of_Total].toFixed(3)}</td>
-                <td>{value[Index_Of_Price]}</td>
-              </tr>
-            ))}
+            .map(([key, value], index) => {
+              return (
+                <tr key={key + index} style={{}}>
+                  <td>{value[Index_Of_Count]}</td>
+                  <td>{value[Index_Of_Amount].toFixed(3)}</td>
+                  <td>
+                    {calculateTotal(
+                      index,
+                      bids.sort(function (a, b) {
+                        if (a[1][3] === b[1][3]) return 0;
+                        else return a[1][3] < b[1][3] ? 1 : -1;
+                      }),
+                      value,
+                      "bids"
+                    )}
+                    {value[Index_Of_Total].toFixed(3)}
+                  </td>
+                  <td>{value[Index_Of_Price]}</td>
+                </tr>
+              );
+            })}
         </table>
 
         {/* asks table */}
-        <table className="Mytable" border={0} style={{}}>
+        <table className="Mytable" border={0} style={{ padding: "10px" }}>
           <tr style={{ backgroundColor: "aqua" }}>
-            <td>Price</td>
-            <td>Total</td>
-            <td>Amount</td>
-            <td>Count</td>
+            <td style={{ height: "30px" }}>Price</td>
+            <td style={{ height: "30px" }}>Total</td>
+            <td style={{ height: "30px" }}>Amount</td>
+            <td style={{ height: "30px" }}>Count</td>
           </tr>
-          {bids
-            .sort(function (a, b) {
-              if (a[1][3] === b[1][3]) {
-                return 0;
-              } else {
-                return a[1][3] < b[1][3] ? -1 : 1;
-              }
-            })
+          {[...asksTableData]
+            .sort(function (currentRow, nextRow) {
+              if (currentRow[1][3] === nextRow[1][3]) return 0;
+               else  return currentRow[1][3] < nextRow[1][3] ? -1 : 1})
             .slice(1, 21)
             .map(([key, value], index) => (
               <tr key={key + index}>
-                <td style={{width:"300px"}}>{value[Index_Of_Price]}</td>
-              
-                   <td>{calculateTotal(index,bids .sort(function (a, b) {
-              if (a[1][3] === b[1][3]) {
-                return 0;
-              } else {
-                return a[1][3] < b[1][3] ? -1 : 1;
-              }
-            }),value,"asks")}{value[Index_Of_Total].toFixed(3)} </td>
-                  {/* <td>{calculateTotal(index,value,"bids",[...bidstest].sort(function (a, b) {
-                    if (a[1][3] === b[1][3]) {
-                      return 0;
-                    } else {
-                      return a[1][3] < b[1][3] ? -1 : 1;
-                    }
-                  }))}</td> */}
-                
+                <td style={{ width: "300px" }}>{value[Index_Of_Price]}</td>
+                <td>
+                  {calculateTotal(
+                    index,
+                    asks.sort(function (a, b) {
+                      if (a[1][3] === b[1][3]) {
+                        return 0;
+                      } else {
+                        return a[1][3] < b[1][3] ? -1 : 1;
+                      }
+                    }),
+                    value,
+                    "asks"
+                  )}
+                  {value[Index_Of_Total].toFixed(3)}{" "}
+                </td>
                 <td>{value[Index_Of_Amount].toFixed(3)}</td>
                 <td>{value[Index_Of_Count]}</td>
               </tr>
             ))}
         </table>
       </div>
-      {/* to close ws connection */}
-      {/* <button onClick={() => w.close()}>close connection</button> */}
     </>
   );
 }
--- src/utils/OrderBookservice.ts
+++ src/utils/OrderBookservice.ts
@@ -4,10 +4,10 @@ import { Index_Of_Amount, Index_Of_Total } from "../types/DataType";
 // export const asksMap=new Map<number,[number,number,number,number]>();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// let minBid = 0;
-//  const maxBid = 0;
-  export const bidsMap = new Map<number, [number, number, number, number]>();
-  export const asksMap = new Map<number, [number, number, number, number]>();
+export let minBid = 0;
+export let maxBid = 0;
+export const bidsMap = new Map<number, [number, number, number, number]>();
+export const asksMap = new Map<number, [number, number, number, number]>();
 const Initial_Total = 0;
 
 export const updateMap = (
@@ -16,15 +16,18 @@ export const updateMap = (
   bidsMap: Map<number, [number, number, number, number]>
 ) => {
   const [price, count, amount] = itemTobeAdd;
-  if(typeof price==="number" && typeof count==="number" && typeof amount ==="number"){
-
+  if (
+    typeof price === "number" && //sometime data is coming in weird format like "1.038399.73783.383" thats why checking type here
+    typeof count === "number" &&
+    typeof amount === "number"
+  ) {
     if (count !== 0) {
       if (amount < 0) {
-        asksMap.set(price, [count, amount, Initial_Total, price]);
+        asksMap.set(price, [count, Math.abs(amount), Initial_Total, price]);
       } else {
-        bidsMap.set(price, [count, amount, Initial_Total, price]);
+        bidsMap.set(price, [count, Math.abs(amount), Initial_Total, price]);
       }
-    }else {
+    } else {
       if (amount > 0) {
         bidsMap.delete(price);
       } else {
@@ -33,62 +36,32 @@ export const updateMap = (
     }
   }
 };
-[[1],[2],[3]]
 
-
-export const calculateTotal=(index:number,list:any[],item:[number,number,number,number],tableType:string):void=>{
-// const prev=list[index-1]
-if(tableType=="bids"){
-
-  if(index===0){
-   item[2]=item[1]
-  }else{
-  item[2]=item[1]+list[index][1][2]
-
-}
-}else{
-  if(index===0){
-    item[2]=item[1]
-   }else{
-   item[2]=item[1]+list[index][1][2]
- 
- }
-}
-}
-
-//  export const calcTotalBid(index: number, itemValue: [number, number, number, number], list: KeyValue<number, [number, number, number, number]>[]): void {
-//     if (index == 0) {
-//       this.minBid = itemValue[this.INDEX_OF.AMOUNT];
-//       itemValue[this.INDEX_OF.TOTAL] = itemValue[this.INDEX_OF.AMOUNT];
-//     } else {
-//       itemValue[this.INDEX_OF.TOTAL] = list[index - 1].value[this.INDEX_OF.TOTAL] + itemValue[this.INDEX_OF.AMOUNT];
-//       if (index == this.LAST_INDEX)
-//         this.maxBid = itemValue[this.INDEX_OF.TOTAL];
-//     }
-//   }
-
-//  export const calcTotalAsk(index: number, itemValue: [number, number, number, number], list: KeyValue<number, [number, number, number, number]>[]): void {
-//     if (index == 0) {
-//       this.minAsk = itemValue[this.INDEX_OF.AMOUNT];
-//       itemValue[this.INDEX_OF.TOTAL] = itemValue[this.INDEX_OF.AMOUNT];
-//     } else {
-//       itemValue[this.INDEX_OF.TOTAL] = list[index - 1].value[this.INDEX_OF.TOTAL] + itemValue[this.INDEX_OF.AMOUNT];
-//       if (index == this.LAST_INDEX)
-//         this.maxAsk = itemValue[this.INDEX_OF.TOTAL];
-//     }
-//   }
-
-// export const calculateAsksTotal = (
-//   index: number,
-//   bidsMapItem: [number, number, number, number]
-// ) => {
-//   return "hehv";
-// };
-type KeyValue = {
-  key: number;
-  value: [number, number, number, number];
+export const calculateTotal = (
+  index: number,
+  list: any[][],
+  item: [number, number, number, number],
+  tableType: string
+): void => {
+  if (tableType == "bids") {
+    if (index === 0) {
+      minBid = item[Index_Of_Amount];
+      item[2] = item[1];
+    } else {
+      if (index == 20) {
+        maxBid = item[Index_Of_Total];
+      }
+      item[2] = item[1] + list[index][1][2];
+    }
+  } else {
+    if (index === 0) {
+      minBid = item[Index_Of_Amount];
+      item[2] = item[1];
+    } else {
+      if (index == 20) {
+        maxBid = item[Index_Of_Total];
+      }
+      item[2] = item[1] + list[index][1][2];
+    }
+  }
 };
-
-// export const updateBidsMap=(setBidsMapData:React.Dispatch<React.SetStateAction<Map<number, [number, number, number, number]>>>)=>{
-//     setBidsMapData(bidsMap)
-// }
