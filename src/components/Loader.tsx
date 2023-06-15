e0eaf8234eefe14fb8ede726c8dfd6cb52038298
arbaz49 <asolkar@deqode.com>
2023-06-15 16:41:45 +0530

    loader Changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,8 +4,8 @@ import Box from '@mui/material/Box';
 
 export default function Loader() {
   return (
-    <Box sx={{ display: 'flex',justifyContent:"center",margin:"auto",marginTop:"200px" }}>
-      <CircularProgress size={200} />
+    <Box sx={{ display: 'flex',justifyContent:"center",margin:"auto",marginTop:"200px",color:"aqua" }}>
+      <CircularProgress size={200} sx={{color:"aqua"}}   />
     </Box>
   );
 }
